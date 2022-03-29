43399326337bdf8cfdd3c3785676d1d7f5367a05
Sourcery AI <>
2022-03-29 18:00:09 +0000

    'Refactored by Sourcery'

--- _action_files/parse_netlify.py
+++ _action_files/parse_netlify.py
@@ -3,5 +3,5 @@ logs = sys.stdin.read()
 
 draft_url = re.findall(r'Website Draft URL: .*(https://.*)', logs)[0]
 assert draft_url, 'Was not able to find Draft URL in the logs:\n{}'.format(logs)
-print("::set-output name=draft_url::{}".format(draft_url))
+print(f"::set-output name=draft_url::{draft_url}")
 
